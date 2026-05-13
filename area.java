8be6aff61b87d7b743029f981aa699bd9278f360
Aarav432-qq <aarav.goyal20077@gmail.com>
2026-05-13 16:18:37 +0530

    updated code

---
+++ area.java
@@ -0,0 +1,10 @@
+class area
+{public static void main(String arr[])
+{
+int a,b,area;
+a=10;
+b=20;
+area=a*b;  
+System.out.println("area of rectangle is"+area);
+}
+}
---
+++ average.java
@@ -0,0 +1,9 @@
+class average
+{public static void main(String arr[])
+{int a,b,c,d;
+a=10;
+b=20;
+c=30;
+d=a+b+c/3;
+System.out.println("average is"+d);}
+}
