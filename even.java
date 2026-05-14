fd1a229fe6fbb69eb3b954b5d1906fc84d84a6e4
Aarav432-qq <aarav.goyal20077@gmail.com>
2026-05-14 13:53:28 +0530

    add even.java

---
+++ even.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class even 
+{public static void main(String arr[])
+{Scanner sc=new Scanner(System.in);
+int n;
+System.out.println("enter a no");
+n=sc.nextInt();
+if(n%2==0)
+System.out.println("the no is even");
+else
+System.out.println("the no is odd");
+}
+}
