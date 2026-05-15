b9388d1fc600721fbfa6ddb8686b049ecb187434
Aarav432-qq <aarav.goyal20077@gmail.com>
2026-05-15 14:05:35 +0530

    add pattern.java

---
+++ pattern.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class pattern
+{public static void main(String arr[])
+{Scanner sc=new Scanner(System.in);
+int n,i,j;
+System.out.println("enter a no");
+n=sc.nextInt();
+for(i=1;i<=n;i++)
+{System.out.println(" ");
+for(j=1;j<=i;j++)
+{System.out.print("*");}
+}
+}
+}
