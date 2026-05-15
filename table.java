4e599b020f8b9ef2ac8d093fc02579ff4a297dfd
Aarav432-qq <aarav.goyal20077@gmail.com>
2026-05-15 13:20:16 +0530

    Add table.java

---
+++ table.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class table
+{public static void main(String arr[])
+{Scanner sc=new Scanner(System.in);
+int n,i,p;
+n=sc.nextInt();
+for(i=0;i<10;i++)
+{p=n*i;
+System.out.println(n+"*"+i +"="+p);}
+
+}
+}
