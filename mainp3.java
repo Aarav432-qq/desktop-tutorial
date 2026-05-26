5d43d68463ca4fac782f725b9f20de76204dc297
Aarav432-qq <aarav.goyal20077@gmail.com>
2026-05-26 13:14:33 +0530

    add mainp3.java

---
+++ mainp3.java
@@ -0,0 +1,14 @@
+class nestingg{
+    int sqaure(int n)
+    {return n*n;
+    }
+    void result()
+    {int ans =sqaure(5);
+    System.out.println("the square is"+ans);
+    }
+}
+class mainp3{
+    public static void main(String arr[])
+    {nestingg object=new nestingg();
+    object.result();
+    }}
