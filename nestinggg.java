c49e131c2faebcbf7f4d068cf349f729e102ead2
Aarav432-qq <aarav.goyal20077@gmail.com>
2026-05-26 13:20:22 +0530

    add nestinggg.java

---
+++ nestinggg.java
@@ -0,0 +1,12 @@
+class nestinggg{
+    int sum(int a,int b)
+    {return a+b;
+    }
+    void result()
+    {int ans=sum(10,20);
+    System.out.println("the sum is"+ans);}
+    public static void main(String arr[])
+    {nestinggg object=new nestinggg();
+    object.result();
+    }
+}
