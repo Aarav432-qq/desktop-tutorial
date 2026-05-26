e3557c9951bd9de8305495bf4a3ade2be2bf4bcb
Aarav432-qq <aarav.goyal20077@gmail.com>
2026-05-26 13:14:55 +0530

    add nesting.java

---
+++ nesting.java
@@ -0,0 +1,12 @@
+class nesting{
+    int sqaure(int n)
+    {return n*n;
+    }
+    void result()
+    {int ans =sqaure(5);
+    System.out.println("the square is"+ans);
+    }
+    public static void main(String arr[])
+    {nesting object=new nesting();
+    object.result();
+    }}
