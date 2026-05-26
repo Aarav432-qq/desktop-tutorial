118f26d3161e3e8d9c0bed065ad9e0257aa4e0a5
Aarav432-qq <aarav.goyal20077@gmail.com>
2026-05-26 13:21:56 +0530

    add mainp4.java

---
+++ mainp4.java
@@ -0,0 +1,13 @@
+class nestinggg{
+    int sum(int a,int b)
+    {return a+b;
+    }
+    void result()
+    {int ans=sum(10,20);
+    System.out.println("the sum is"+ans);}
+}
+class mainp4{
+    public static void main(String arr[])
+    {nestinggg object=new nestinggg();
+    object.result();
+    }}
