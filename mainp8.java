d07bbfea7718476118f4cfdf250c300bd82b9058
Aarav432-qq <aarav.goyal20077@gmail.com>
2026-05-28 13:56:08 +0530

    add mainp8,java

---
+++ mainp8.java
@@ -0,0 +1,15 @@
+class time
+{int h,m;
+time()
+{h=2;
+m=30;}
+void showdata()
+{System.out.println("time is"+h+":"+m);
+}
+}
+class mainp8
+{public static void main(String arr[])
+{time t1=new time();
+t1.showdata();
+}
+}
