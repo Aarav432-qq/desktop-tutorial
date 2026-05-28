78880eb7fc6f52d3892fe0b72288a4c818d10487
Aarav432-qq <aarav.goyal20077@gmail.com>
2026-05-28 13:58:23 +0530

    add main9.java

---
+++ main9.java
@@ -0,0 +1,17 @@
+class distance
+{int f,i;
+distance()
+{f=5;
+i=10;
+
+}
+void showdata()
+{System.out.println("distance is"+f+"feet and"+i+"inches");
+}
+}
+class main9
+{public static void main(String arr[])
+{distance d1=new distance();
+d1.showdata();
+}
+}
