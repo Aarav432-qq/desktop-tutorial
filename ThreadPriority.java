01b65f42228a11fd3afc95ec6dd11aaadfa9121b
Aarav432-qq <aarav.goyal20077@gmail.com>
2026-06-06 13:22:11 +0530

    add ThreadPriority.java

---
+++ ThreadPriority.java
@@ -0,0 +1,23 @@
+class MyThread extends Thread
+{public MyThread(String name)
+{super(name);
+}
+public void run()
+{for(int i=1;i<5;i++)
+    {System.out.println(getName()+"priority"+getPriority()+"count is"+i);
+    }
+}
+}
+class ThreadPriority
+{public static void main(String args[])
+{MyThread t1=new MyThread("Thread 1");
+MyThread t2=new MyThread("Thread 2");
+MyThread t3=new MyThread("Thread 3");
+t1.setPriority(Thread.MIN_PRIORITY);
+t2.setPriority(Thread.NORM_PRIORITY);
+t3.setPriority(Thread.MAX_PRIORITY);
+t1.start();
+t2.start();
+t3.start();
+}
+}
