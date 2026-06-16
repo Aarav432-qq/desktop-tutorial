9d0d0f1209ac647dc1f758616b8d43268fa6f86f
Aarav432-qq <aarav.goyal20077@gmail.com>
2026-06-16 13:53:38 +0530

    add readfile4.java

--- readfile4.java
+++ readfile4.java
@@ -4,16 +4,20 @@ class readfile4
 {public static void main(String arr[])
 {try
 {FileReader fr=new FileReader("prime.java");
-int ch,v=0,s=0;
+int ch,v=0,s=0,l=0;
 while((ch= fr.read())!=-1)
 {System.out.print((char)ch);
 if(ch=='a' ||ch=='e'||ch=='o'||ch=='i'||ch=='u')
 v++;
+if (ch=='\n')
+l++;
 s++;
+
 
 }
 System.out.println("no of vowels are:"+v);
 System.out.println("size of file:"+s);
+System.out.println("no of line are:"+l);
 fr.close();
 }
 catch(IOException e)
